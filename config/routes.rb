28eaee8edeac058a19f1dcd2127c9cf4f5f549f9
stephen choi <stephenjhchoi@gmail.com>
2017-03-01 18:57:40 +0000

    created dashboards controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  get 'dashboards/show'
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   mount Attachinary::Engine => "/attachinary"
