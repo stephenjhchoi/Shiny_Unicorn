b007ecb92e37f7bf13ada1676cf7061b3ff0efb3
Takahiro Hasegawa <takahiro.taka.hasegawa@gmail.com>
2017-03-05 17:56:50 +0000

    fixed

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -7,8 +7,6 @@ class DashboardsController < ApplicationController
     @venues = @venues.joins(:categories).where(categories: {title: session[:categories]}) if session[:categories].blank?
     @recommended_venues = @venues.uniq
 
-    @bookings = Booking.where(user_id: current_user.id)
-    raise
     @hash = Gmaps4rails.build_markers(current_user.venues) do |venue, marker|
       marker.lat venue.latitude
       marker.lng venue.longitude
