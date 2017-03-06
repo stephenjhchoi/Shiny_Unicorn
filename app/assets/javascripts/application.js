5730cdf5a0a602725352759b4a5bce9ac04357ab
Takahiro Hasegawa <takahiro.taka.hasegawa@gmail.com>
2017-03-06 15:18:31 +0000

    create review controller and review form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,20 +12,6 @@
 //= require_tree .
 
 
-$( document ).ready(function() {
-$("#ex9").slider({
-  precision: 1,
-  value: 0 // Slider will instantiate showing 8.12 due to specified precision
-});
-});
-
-
-$( document ).ready(function() {
-  $("#ex11").slider({step: 1, min: 0, max: 10
-  });
-});
-
-
 $('.venue-glow-btn').on('click', function() {
   $(this).toggleClass("active");
 });
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,20 @@
 class ReviewsController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index,:show]
+  skip_after_action :verify_policy_scoped
+  skip_after_action :verify_authorized
+  def create
+    @venue= Venue.find(params[:venue_id])
+    @review = Review.new(review_params)
+    @review.venue = @venue
+    @review.user = current_user
+    if @review.save
+      redirect_to venue_path(@review.venue)
+    end
+  end
 
+  private
+
+  def review_params
+    params.require(:review).permit(:hotness_level, :description, :venue_id)
+  end
 end
