50b0a6f9149374b037442b3fe5d0cc5eb3704a8f
stephen choi <stephenjhchoi@gmail.com>
2017-03-07 17:36:52 +0000

    eliminated om line in dshbd ctrl

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -10,10 +10,6 @@ class DashboardsController < ApplicationController
     @hash = Gmaps4rails.build_markers(current_user.venues) do |venue, marker|
       marker.lat venue.latitude
       marker.lng venue.longitude
-      marker.infowindow = "<%= @venues.first.name %>"
-
-
-
     end
   end
 end
