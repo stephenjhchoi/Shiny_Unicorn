45c510b7614722aa204a3f2877f3e18154be5b9e
stephen choi <stephenjhchoi@gmail.com>
2017-03-08 12:19:06 +0000

    resized unicorn pin

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -30,8 +30,8 @@ class VenuesController < ApplicationController
       marker.lng venue.longitude
       marker.picture({
                   url: view_context.image_path('unicorn-icon.svg'),
-                  width: 100,
-                  height: 100
+                  width: 50,
+                  height: 50
                  })
       marker.infowindow render_to_string(partial: "/venues/map_box", locals: { venue: venue })
       marker.json({ id: venue.id })
